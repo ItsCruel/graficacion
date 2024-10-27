75a0a95b6dfe46a452a594fe60b7e4f4832cb2c4
ItsCruel <cruel34530@gmail.com>
2024-10-27 15:52:30 -0600

    commit practica 5 modificaciones

--- practica5.py
+++ practica5.py
@@ -8,8 +8,17 @@ imagen = np.zeros((500,500 , 3), dtype="uint8")
 cv2.circle(imagen,(250,150),50,(255,0,0), -1)
 
 #Dibujar el cuerpo (rectangulo)
-cv2.rectangle(imagen,(220,200),(280,350),(255,0,0)-1)
+cv2.rectangle(imagen,(220,200),(280,350),(255,0,0),-1)
 
 #Dibujar brazos
-cv2.line(imagen, (220 , 220),(150,300))
-cv2.line
+cv2.line(imagen, (220 , 220),(150,300),(255,0,0),5)
+cv2.line(imagen, (280 , 220),(350,300),(255,0,0),5)
+
+#Dibujar las piernas 
+cv2.line(imagen,(240 , 350),(200,450),(255,0,0),5)#pierna izquierda
+cv2.line(imagen,(260 , 350),(300,450),(255,0,0),5)#pierna derecha
+
+#Monstrando resultado
+cv2.imshow('persona',imagen)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
