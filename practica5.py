d7624882ef02b1e51b7f911e425e5e43974c8191
ItsCruel <cruel34530@gmail.com>
2024-10-27 09:42:33 -0600

    commit practica5 .py 2

--- practica5.py
+++ practica5.py
@@ -1,1 +1,15 @@
-1 + A= 3
+import cv2
+import numpy as np
+
+#Crear una imagen en blanco de 500 x 500
+imagen = np.zeros((500,500 , 3), dtype="uint8")
+
+#Dibujar cabeza (circulo)
+cv2.circle(imagen,(250,150),50,(255,0,0), -1)
+
+#Dibujar el cuerpo (rectangulo)
+cv2.rectangle(imagen,(220,200),(280,350),(255,0,0)-1)
+
+#Dibujar brazos
+cv2.line(imagen, (220 , 220),(150,300))
+cv2.line
