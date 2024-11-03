bec2fe3c32bf90d0f56328aaba7ac2e8ecf5d8fc
ItsCruel <cruel34530@gmail.com>
2024-11-03 07:02:38 -0600

    commit primer commit de pelota pyy

--- pelota.py
+++ pelota.py
@@ -1,1 +1,13 @@
-dhdh
+import cv2
+import numpy as np
+
+# Dimensiones de la ventana
+ancho, alto = 640, 480
+
+# Crear ventana
+cv2.namedWindow("Animacion", cv2.WINDOW_AUTOSIZE)
+
+# Posicion inicial
+pos_pelota = np.array([100, 100])
+radio_pelota = 20
+velocidad = np.array([5, 3])
