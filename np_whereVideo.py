ffe97a77151b257df05395f91e0946a1453fe073
ItsCruel <cruel34530@gmail.com>
2024-11-02 23:39:35 -0600

    npwherevideo primer commit

--- np_whereVideo.py
+++ np_whereVideo.py
@@ -1,1 +1,8 @@
-np_whereVideo
+import cv2
+import numpy as np
+
+# Abrir la camara 
+video = cv2.VideoCapture(0)
+
+# Convertir el cuadro de BGR a HSV
+frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
