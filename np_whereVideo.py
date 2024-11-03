f988f9f7258becd64683a2f6794fb87848221ad6
ItsCruel <cruel34530@gmail.com>
2024-11-03 06:54:57 -0600

    np whereVideo p2 funcionando

--- np_whereVideo.py
+++ np_whereVideo.py
@@ -17,3 +17,17 @@ alto_rojo2 = np.array([180, 255, 255])
 mascara_rojo1 = cv2.inRange(frame_hsv, bajo_rojo1, alto_rojo1)
 mascara_rojo2 = cv2.inRange(frame_hsv, bajo_rojo2, alto_rojo2)
 mascara_rojo = cv2.add(mascara_rojo1, mascara_rojo2)
+ #cuadro original a escala de grises
+frame_gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+#  imagen gris a un formato BGR 
+frame_gris_bgr = cv2.cvtColor(frame_gris, cv2.COLOR_GRAY2BGR)
+    
+# combinar el cuadro en gris con las areas en rojo
+resultado = np.where(mascara_rojo[:, :, None] == 255, frame, frame_gris_bgr)
+
+cv2.imshow('Color resaltadoo', resultado)
+cv2.waitKey(0)
+ 
+video.release()
+cv2.destroyAllWindows()
