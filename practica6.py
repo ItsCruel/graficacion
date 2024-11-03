be7a88ddd56531842f1fe77b4eac8141a11037cd
ItsCruel <cruel34530@gmail.com>
2024-11-02 22:20:38 -0600

    commit practica 6 commit 6 p2

--- practica6.py
+++ practica6.py
@@ -17,3 +17,60 @@ for eq in range(10):
         x = 180 * np.cos(t)
         y = 180 * np.sin(t)
     elif eq == 1:
+          # Elipse
+        x = 220 * np.cos(t)
+        y = 120 * np.sin(t)
+    elif eq == 2:
+        # espiral logaritmica
+        r = np.exp(0.1 * t)
+        x = r * np.cos(t)
+        y = r * np.sin(t)
+    elif eq == 3:
+        # Lemniscata
+        x = 180 * np.cos(t) / (1 + np.sin(t)**2)
+        y = 180 * np.sin(t) * np.cos(t) / (1 + np.sin(t)**2)
+    elif eq == 4:
+        # Rosa con 3 petalos
+        k = 3
+        r = 180 * np.cos(k * t)
+        x = r * np.cos(t)
+        y = r * np.sin(t)
+    elif eq == 5:
+        # Cardioide
+        x = 180 * (1 - np.cos(t)) * np.cos(t)
+        y = 180 * (1 - np.cos(t)) * np.sin(t)
+    elif eq == 6:
+        # Hipociclo
+        R, r, d = 180, 40, 40
+        x = (R - r) * np.cos(t) + d * np.cos(((R - r) / r) * t)
+        y = (R - r) * np.sin(t) - d * np.sin(((R - r) / r) * t)
+    elif eq == 7:
+        # Espiral de arquimedes
+        a, b = 1, 15
+        r = a + b * t
+        x = r * np.cos(t)
+        y = r * np.sin(t)
+    elif eq == 8:
+        # Asteroide
+        x = 180 * (np.cos(t)**3)
+        y = 180 * (np.sin(t)**3)
+    elif eq == 9:
+        # Hiperbola
+        x = 180 * np.sinh(t)
+        y = 130 * np.cosh(t)
+
+
+    # Mover las coordenadas al centro de la imagen
+    x = np.int32(centro_x + x)
+    y = np.int32(centro_y - y)
+
+
+    # Dibujar las lineas entre los puntos
+    for j in range(len(x) - 1):
+        cv2.line(img, (x[j], y[j]), (x[j + 1], y[j + 1]), (255, 255, 255), 2)
+
+
+# Mostrar la imagen con las curvas
+cv2.imshow("curvAs Parametrica", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
