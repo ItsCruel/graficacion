6ac1c6ecbee82ef29cd32e5e6d5b561f472143ea
ItsCruel <cruel34530@gmail.com>
2024-11-03 09:19:13 -0600

    MatrizSobel funcionando

--- MatrizSobel.py
+++ MatrizSobel.py
@@ -19,10 +19,10 @@ for i in range(alto):
     for j in range(ancho):
         ImagenEscalada[i * escala_y, j * escala_x] = imagen[i, j]
 
-# Obtener el tamaño 
+# Obtener el tamano 
 alto_escalado, ancho_escalado = ImagenEscalada.shape
 
-#matriz de Sobel para detectar bordes
+#matriz de Sobel para bordes
 for i in range(alto_escalado):
     for j in range(ancho_escalado):
         
@@ -38,4 +38,58 @@ for i in range(alto_escalado):
         else:
             pixel = int(ImagenEscalada[i][j - 1])
         gradiente += pixel * 0
+
+        if (i + 1) >= alto_escalado or (j - 1) < 0:
+            pixel = 0
+        else:
+            pixel = int(ImagenEscalada[i + 1][j - 1])
+        gradiente += pixel * 1
+
+        if (i - 1) < 0:
+            pixel = 0
+        else:
+            pixel = int(ImagenEscalada[i - 1][j])
+        gradiente += pixel * (-2)
+
+        pixel = int(ImagenEscalada[i][j])
+        gradiente += pixel * 0
+
+        if (i + 1) >= alto_escalado:
+            pixel = 0
+        else:
+            pixel = int(ImagenEscalada[i + 1][j])
+        gradiente += pixel * 2
+
+        if (i - 1) < 0 or (j + 1) >= ancho_escalado:
+            pixel = 0
+        else:
+            pixel = int(ImagenEscalada[i - 1][j + 1])
+        gradiente += pixel * (-1)
+
+        if (j + 1) >= ancho_escalado:
+            pixel = 0
+        else:
+            pixel = int(ImagenEscalada[i][j + 1])
+        gradiente += pixel * 0
+
+        if (i + 1) >= alto_escalado or (j + 1) >= ancho_escalado:
+            pixel = 0
+        else:
+            pixel = int(ImagenEscalada[i + 1][j + 1])
+        gradiente += pixel * 1
+
+        # limitar los valores 
+        if gradiente > 255:
+            gradiente = 255
+        if gradiente < 0:
+            gradiente = 0
+
+        # Asignar el valor del gradiente en la posicion
+        ImagenEscalada[i][j] = int(gradiente)
+
+# Mostrar la imagen original y la imagen escalada
+cv.imshow('Imagen original', imagen)
+cv.imshow('Imagen escalada', ImagenEscalada)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
