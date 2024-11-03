5b3e08ddb20a8852fcb03f2d4963108f4fa8d1b9
ItsCruel <cruel34530@gmail.com>
2024-11-03 07:14:14 -0600

    segundo commit pelota

--- pelota.py
+++ pelota.py
@@ -11,3 +11,20 @@ cv2.namedWindow("Animacion", cv2.WINDOW_AUTOSIZE)
 pos_pelota = np.array([100, 100])
 radio_pelota = 20
 velocidad = np.array([5, 3])
+
+# Color de la pelotita (azul, verde, rojo)
+color_pelota = (255, 0, 0)
+
+# Bucle de animacion
+while True:
+    fotograma = np.zeros((alto, ancho, 3), dtype=np.uint8)
+    
+    # Dibuja la pelotita
+    cv2.circle(fotograma, pos_pelota, radio_pelota, color_pelota, -1)
+    
+    pos_pelota += velocidad
+    
+    # Comprobar colision y cambia la direccion
+    if pos_pelota[0] - radio_pelota <= 0 or pos_pelota[0] + radio_pelota >= ancho:
+        velocidad[0] = -velocidad[0]
+        
