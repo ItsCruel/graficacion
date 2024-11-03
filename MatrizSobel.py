d9f562339d1d76573c34036dc13a70876567442f
ItsCruel <cruel34530@gmail.com>
2024-11-03 08:23:58 -0600

    avance de matrizSobel

--- MatrizSobel.py
+++ MatrizSobel.py
@@ -10,7 +10,32 @@ alto, ancho = imagen.shape
 # factor de escala
 escala_x, escala_y = 2, 2
 
-# nueva imagen para el escalado
-imagen_escalada = np.zeros((int(alto * escala_y), int(ancho * escala_x)), dtype=np.uint8)
+# imagen para el escalado
+ImagenEscalada = np.zeros((int(alto * escala_y), int(ancho * escala_x)), dtype=np.uint8)
 
+
+# imagen duplicando pixeles
+for i in range(alto):
+    for j in range(ancho):
+        ImagenEscalada[i * escala_y, j * escala_x] = imagen[i, j]
+
+# Obtener el tamaño 
+alto_escalado, ancho_escalado = ImagenEscalada.shape
+
+#matriz de Sobel para detectar bordes
+for i in range(alto_escalado):
+    for j in range(ancho_escalado):
+        
+        gradiente = 0
+        if (i - 1) < 0 or (j - 1) < 0:
+            pixel = 0
+        else:
+            pixel = int(ImagenEscalada[i - 1][j - 1])
+        gradiente += pixel * (-1)
+
+        if (j - 1) < 0:
+            pixel = 0
+        else:
+            pixel = int(ImagenEscalada[i][j - 1])
+        gradiente += pixel * 0
 
