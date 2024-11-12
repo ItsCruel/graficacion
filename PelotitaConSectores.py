b9e788f146e710ca68b90c44c74c7496b533a59f
ItsCruel <cruel34530@gmail.com>
2024-11-12 12:31:10 -0600

    PelotitaConSectores Commit

--- PelotitaConSectores.py
+++ PelotitaConSectores.py
@@ -1,27 +1,26 @@
 import numpy as np
 import cv2 as cv
 
-
 # dimensiones de la ventana
 ancho, alto = 640, 480
 
 # crear ventana
 cv.namedWindow("Animacion", cv.WINDOW_AUTOSIZE)
 
-# posicion inicial
+# posicion inicial de la pelota
 pos_pelota = np.array([100, 100])
 radio_pelota = 20
 velocidad = np.array([5, 3])
 
-# Color fijo de la pelotita 
+# color fijo de la pelota
 color_pelota = (255, 0, 0)
 
-# Bucle de animacion
+# bucle de animacion
 while True:
-    # crear fondo
+    # Crear fondo
     fotograma = np.zeros((alto, ancho, 3), dtype=np.uint8)
     
-    # dibujar divisiones
+    # dibujar divisiones para los sectores
     cv.line(fotograma, (ancho // 2, 0), (ancho // 2, alto), (255, 255, 255), 2)
     cv.line(fotograma, (0, alto // 2), (ancho, alto // 2), (255, 255, 255), 2)
     
@@ -35,27 +34,29 @@ while True:
     else:
         sector = 4
 
-    # dibujar la pelotita
+    # Dibujar la pelotita
     cv.circle(fotograma, tuple(pos_pelota), radio_pelota, color_pelota, -1)
     
-    # mostrar el sector en la ventana
+    # Mostrar el sector en el que se encuentra la pelotita
     texto_sector = f"Sector: {sector}"
-    cv.putText(fotograma, texto_sector, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv.putText(fotograma, texto_sector, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     
-    # actualizar posicion de la pelota
+    # Actualizar la posicion de la pelota
     pos_pelota += velocidad
     
-    # comprobar colisiones y cambiar direccion
+    # Comprobar colisiones y cambiar direccion
     if pos_pelota[0] - radio_pelota <= 0 or pos_pelota[0] + radio_pelota >= ancho:
         velocidad[0] = -velocidad[0]
 
     if pos_pelota[1] - radio_pelota <= 0 or pos_pelota[1] + radio_pelota >= alto:
         velocidad[1] = -velocidad[1]
     
-    # Mostrar imagen
+    # Mostrar la imagen en la ventana
     cv.imshow("Animacion", fotograma)
 
-    # Salir 
+    # Salir al presionar 'Esc'
     if cv.waitKey(1) & 0xFF == 27:
         break
+
+# Cerrar todas las ventanas de OpenCV
 cv.destroyAllWindows()
