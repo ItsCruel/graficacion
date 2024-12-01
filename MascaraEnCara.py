752e9fe7a3a0e93c1ef7f0e2c93f6314e9df39da
ItsCruel <cruel34530@gmail.com>
2024-11-30 18:11:51 -0600

    Commit de mascara py

--- MascaraEnCara.py
+++ MascaraEnCara.py
@@ -3,26 +3,41 @@ import cv2 as cv
 
 rostro = cv.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 cap = cv.VideoCapture(0)
-x=y=w=h= 0 
-count = 0
+
+
+mascara = cv.imread('mascara.png', cv.IMREAD_UNCHANGED)
+
 while True:
     ret, frame = cap.read()
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     rostros = rostro.detectMultiScale(gray, 1.3, 5)
-    for(x, y, w, h) in rostros:
-        m= int(h/2)
-        frame = cv.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
-        #frame = cv.rectangle(frame, (x,y+m), (x+w, y+h), (255, 0 ,0), 2 )
-        #img = 180- frame[y:y+h,x:x+w]
-        #count = count + 1   
-    
-    #name = '/home/likcos/imgs/cara'+str(count)+'.jpg'
-    #cv.imwrite(name, frame)
+
+    for (x, y, w, h) in rostros:
+        # Redimensionar la mascara al tamaño del rostro detectado
+        resized_mascara = cv.resize(mascara, (w, h))
+
+        
+        if resized_mascara.shape[2] == 4:
+            b, g, r, alpha = cv.split(resized_mascara)
+            overlay_color = cv.merge((b, g, r))
+            mask_inv = cv.bitwise_not(alpha)
+        else:
+            overlay_color = resized_mascara
+            mask_inv = np.ones((h, w), dtype="uint8") * 255
+
+     
+        roi = frame[y:y+h, x:x+w]
+        background = cv.bitwise_and(roi, roi, mask=mask_inv)
+        foreground = cv.bitwise_and(overlay_color, overlay_color, mask=alpha if resized_mascara.shape[2] == 4 else mask_inv)
+
+
+        combined = cv.add(background, foreground)
+        frame[y:y+h, x:x+w] = combined
+
     cv.imshow('rostros', frame)
-    #cv.imshow('cara', img)
-    
     k = cv.waitKey(1)
-    if k == 27:
+    if k == 27:  
         break
+
 cap.release()
 cv.destroyAllWindows()
