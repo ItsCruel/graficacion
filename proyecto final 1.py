7307fec4722ecd2966f8cc378ef526d6ce981284
ItsCruel <cruel34530@gmail.com>
2024-12-02 09:22:55 -0600

    proyecto final1 y 2 intento

--- proyecto final 1.py
+++
@@ -1,8 +0,0 @@
-proyecto final 1
-
-por medio del flujo optico (con gestos de la mano) o patrones de movimiento applicar escalamiento , traslacion  y rotacion de cualquier figura
-
- 
-proyecto final 2
-modelo de ciudad y por medio de movimmientos con lla manos podemos rotar , escalamiento , y traslacion
-con hacer una casa podemos hacer todo el pueblo
